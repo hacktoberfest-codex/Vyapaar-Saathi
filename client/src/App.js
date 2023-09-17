c863e68544f34bc52e870aa7415ca22bcd5d2869
Rafi <rafi9641016156@gmail.com>
2023-09-17 05:35:36 +0530

    sixth commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,34 @@
-
-
-
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import HelpAs from './components/HelpAs/HelpAs'
+import Market from './components/Market/Market'
+import Login from './components/Login/Login'
+import Signup from './components/Signup/Signup'
+import Footer from './components/Footer/Footer'
+import MarketMessage from './components/Market/MarketMessage'
 
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 const App = () => {
   return (
-    <div>App</div>
+  <>
+     <BrowserRouter>
+       <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/About" element={<About />}></Route>
+      <Route path="/HelpAs" element={<HelpAs />}></Route>
+      <Route path="/Market" element={<Market />}></Route>
+      <Route path="/signin" element={<Login />}></Route>
+      <Route path="/signup" element={<Signup />}></Route>
+      <Route path="/MarketMessage" element={<MarketMessage />}></Route>
+     
+    </Routes>
+<Footer />
+  </BrowserRouter>
+  </>
   )
 }
 
---
+++ client/src/components/Market/Market.jsx
@@ -0,0 +1,98 @@
+
+import React, { useState, useEffect } from 'react';
+import hwimg from "../../assets/hwb_img.png"
+import './Market.css';
+
+function Market() {
+  const [marketData, setMarketData] = useState([]);
+   const [usermessage, setUserMessage] = useState({
+    textMessage:'',
+    email: '',
+    phone: '',
+     });
+
+     let name,value;
+  const handleMessage=(e)=>{
+    name=e.target.name;
+    value=e.target.value;
+    setUserMessage({...usermessage,[name]:value})
+  }
+
+  const submitMessage = async(e) => {
+    e.preventDefault();
+    const{textMessage,email,phone}=usermessage;
+
+   try {
+    const res=await fetch("/Market",{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify({
+        textMessage,email,phone
+      })
+    })
+    const data = await res.json();
+    if(res.status===422 ||!data){
+      window.alert("Wrong credention");
+      console.log("Invalid Registration");
+    }else{
+      window.alert("Your request send");
+     
+      console.log("Registration Succesfull");
+
+    
+    }
+    
+   } catch (error) {
+    console.error('Error during registration:', error);
+   }
+
+    
+  };
+
+
+
+
+  useEffect(() => {
+    // Fetch data from your backend API here
+    fetch('/Market')
+      .then((response) => response.json())
+      .then((data) => {
+        setMarketData(data);
+      })
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div className="market_main">
+      <div className="cards-container">
+        {marketData.map((item, index) => (
+          <div className="card" key={index}>
+            <div className='card_body'>
+              <div className='market_img'>
+                <img src={hwimg} alt="server_fault"/>
+              </div>
+              <div className='carditems'>
+                <p><span className='dec'>Name: </span><span className='text_span'>&nbsp;{item.name}</span></p>
+                <p><span className='dec'>WorkCategory: </span><span className='text_span1'>{item.work}</span></p>
+                <p><span className='dec'>Contact No: </span><span className='text_span1'>{item.phone}</span></p>
+                <p><span className='dec'>Address: </span><span className='text_span1'>{item.state},{item.city},{item.pincode}</span></p>
+          
+                <p><span className='dec'>Time: </span><span className='text_span'>10AM-6PM</span></p>
+            <input type='text' name='textMessage' placeholder='Add Instruction' className='input-box'  value={usermessage.textMessage} onChange={handleMessage}/>
+            <input type='email' name='email'placeholder='Enter email' className='input-box' value={usermessage.email} onChange={handleMessage} />
+            <input type='phone' name='phone'placeholder='Enter contact no' className='input-box'  value={usermessage.phone} onChange={handleMessage }/>
+            <button className="submit-button" onClick={submitMessage}>
+              Submit
+            </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Market;
